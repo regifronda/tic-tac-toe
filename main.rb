64056a415efc8d73bd78be3f9f5d4918b0b12367
Regi Fronda <regisfronda@gmail.com>
2021-06-24 23:54:11 -0700

    Add class TicTacToe

--- main.rb
+++ main.rb
@@ -1,16 +1,14 @@
 #Set up the game itself [TicTacToe]by
+class TicTacToe
   #Initalize the game board [Board]
   #Initialize two players [Player1 and Player2]
-#Start gameplay loop [TicTacToeLoop]
+end
+#Start gameplay loop [TicTacToe]
   #Display the board [Board]
-  #Ask for player input [PlayerInput]
-  #Check if input is valid
-  #If there's a draw or win [TicTacToeLoop]
+  #Ask for player input and check if formatted correctly[PlayerInput]
+  #If there's a draw or win [TicTacToe]
     #Print victory or draw message
     #Stop looping
   #Else
     #Switch to next player
-    #resume loop [TicTacToeLoop]
-class Player
-  attr_reader 
-end
+    #resume loop [TicTacToe]

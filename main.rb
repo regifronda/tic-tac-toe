41f8ed228df6e1b89ca7619c5caafad59d5cd1f1
Regi Fronda <regisfronda@gmail.com>
2021-06-25 00:03:28 -0700

    Add initialize method to TicTacToe

--- main.rb
+++ main.rb
@@ -1,8 +1,18 @@
-#Set up the game itself [TicTacToe]by
 class TicTacToe
+  def initialize
+    @board = Board.new
+
+  end
+end
+
+class Player
+end
+
+class Board
+end
+#Set up the game itself [TicTacToe]by
   #Initalize the game board [Board]
   #Initialize two players [Player1 and Player2]
-end
 #Start gameplay loop [TicTacToe]
   #Display the board [Board]
   #Ask for player input and check if formatted correctly[PlayerInput]

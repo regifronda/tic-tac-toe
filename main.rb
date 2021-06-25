914f3dd3718b3821c08e8afb3af221db5e7cecc6
Regi Fronda <regisfronda@gmail.com>
2021-06-24 23:18:58 -0700

    Add pseudocode for gameplay loop

--- main.rb
+++ main.rb
@@ -1,6 +1,16 @@
 #Set up the game itself by
   #Initalize the game board
   #Initialize two players
+#Start gameplay loop
+  #Display the board
+  #Ask for player input
+  #Check if input is valid
+  #If there's a draw or win
+    #Print victory or draw message
+    #Stop looping
+  #Else
+    #Switch to next player
+    #resume loop
 class Player
   attr_reader 
 end

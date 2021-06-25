8528dce44b8e4fe61434d5e49a77e2e40700e539
Regi Fronda <regisfronda@gmail.com>
2021-06-24 23:49:11 -0700

    Add names to class

--- main.rb
+++ main.rb
@@ -1,16 +1,16 @@
-#Set up the game itself by
-  #Initalize the game board
-  #Initialize two players
-#Start gameplay loop
-  #Display the board
-  #Ask for player input
+#Set up the game itself [TicTacToe]by
+  #Initalize the game board [Board]
+  #Initialize two players [Player1 and Player2]
+#Start gameplay loop [TicTacToeLoop]
+  #Display the board [Board]
+  #Ask for player input [PlayerInput]
   #Check if input is valid
-  #If there's a draw or win
+  #If there's a draw or win [TicTacToeLoop]
     #Print victory or draw message
     #Stop looping
   #Else
     #Switch to next player
-    #resume loop
+    #resume loop [TicTacToeLoop]
 class Player
   attr_reader 
 end

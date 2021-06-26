4cc60c71c64a0065535e303589b60b968a16ad88
Regi Fronda <regisfronda@gmail.com>
2021-06-25 17:42:13 -0700

    Add more specific pseudocode to Board class

--- main.rb
+++ main.rb
@@ -58,7 +58,58 @@ class Player
 end
 
 class Board
+  #initialize board
+    #set up blank data structure
+
+  #render 
+    #loop through data structure
+      #display an existing marker if any, else blank
+  
+  #add_piece
+    #if piece_location_valid?
+      #place piece
+    #else
+      #print error message
+    
+  #piece_location_valid?
+    #is the input within_valid_coordinates?
+    #are the piece coordinates_free?
+  
+  #within_valid_coordinates?
+    #unless piece coordinates are in the acceptible range
+      #display an error message
+
+  #coordinates_free?
+    #unless piece coordinates are not occupied
+      #display error message
+  
+  #winning_combination
+    #is there a winning_diagonal?
+    #or winning_vertical?
+    #or winning_horizontal? for that piece?
+  
+  #winning_diagonal?
+    #check if there's three of the specified piece across diagonals
+  
+  #winning_vertical?
+    #check if there's three of the specified piece across verticals
+  
+  #winning_horizontal
+    #check if there's three of the specified piece across
+  
+  #diagonals
+    #return diagonal pieces
+  
+  #verticals
+    #return vertical pieces
+  
+  #horizontals
+    #return horizontal pieces
+  
+  #full?
+    #does every square have a piece?
 end
+
 #Set up the game itself [TicTacToe]by
   #Initalize the game board [Board]
   #Initialize two players [Player1 and Player2]

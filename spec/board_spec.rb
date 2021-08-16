945ddd13734d1b0813e0365001e292803e191f96
Regi Fronda <regisfronda@gmail.com>
2021-08-16 08:47:19 -0700

    Add tests for winning_vertical method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,8 +1,72 @@
 require './lib/board'
+require './lib/player'
+require './lib/tictactoe'
 
 describe Board do
   subject(:board) { described_class.new }
   
+  describe "#winning_vertical?" do
+    
+    context "when the board is empty" do
+      before do
+        board.instance_variable_set(:@board, [[nil, nil, nil], [nil, nil, nil], [nil, nil, nil]])
+      end
+      
+      it "returns false" do
+        test_piece = :x
+        vertical_check = board.winning_vertical?(test_piece)
+        expect(vertical_check).to be_falsy
+      end
+    end
+
+    context "when 2 of 3 vertical squares have the same piece" do
+      before do
+        board.instance_variable_set(:@board, [[:x, nil, nil], [:x, nil, nil], [nil, nil, nil]])
+      end
+      
+      it "returns false" do
+        test_piece = :x
+        vertical_check = board.winning_vertical?(test_piece)
+        expect(vertical_check).to be_falsy
+      end
+    end
+
+    context "when the left column has 3 of the same piece" do
+      before do
+        board.instance_variable_set(:@board, [[:x, nil, nil], [:x, nil, nil], [:x, nil, nil]])
+      end
+      
+      it "returns true" do
+        test_piece = :x
+        vertical_check = board.winning_vertical?(test_piece)
+        expect(vertical_check).to be_truthy
+      end
+    end
+
+    context "when the middle column has 3 of the same piece" do
+      before do
+        board.instance_variable_set(:@board, [[nil, :x, nil], [nil, :x, nil], [nil, :x, nil]])
+      end
+      
+      it "returns true" do
+        test_piece = :x
+        vertical_check = board.winning_vertical?(test_piece)
+        expect(vertical_check).to be_truthy
+      end
+    end
+
+    context "when the right column has 3 of the same piece" do
+      before do
+        board.instance_variable_set(:@board, [[nil, nil, :x], [nil, nil, :x], [nil, nil, :x]])
+      end
+      
+      it "returns true" do
+        test_piece = :x
+        vertical_check = board.winning_vertical?(test_piece)
+        expect(vertical_check).to be_truthy
+      end
+    end
+  end
   describe "#full?" do
     context "when board is empty" do
       before do

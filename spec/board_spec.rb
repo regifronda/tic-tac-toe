6c0b5db9ce533e1067fda732f3c613c265307162
Regi Fronda <regisfronda@gmail.com>
2021-08-16 06:57:45 -0700

    Add tests for partially and completely filled out board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -13,5 +13,25 @@ describe Board do
         expect(board).not_to be_full
       end
     end
+
+    context "when board is partially filled out" do
+      before do
+        board.instance_variable_set(:@board, [[nil, :o, nil], [:x, nil, nil], [:x, nil, nil]])
+      end
+
+      it "is not full" do
+        expect(board).not_to be_full
+      end
+    end
+
+    context "when board is completely filled out" do
+      before do
+        board.instance_variable_set(:@board, [[:x, :o, :x], [:x, :x, :o], [:o, :o, :x]])
+      end
+
+      it "is is full" do
+        expect(board).to be_full
+      end
+    end
   end
 end

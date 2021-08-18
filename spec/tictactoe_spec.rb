dfdc3494c4de5c9b5f9299349c004f74b7795695
Regi Fronda <regisfronda@gmail.com>
2021-08-17 20:27:23 -0700

    Add space between tests

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -1,15 +1,18 @@
 require './lib/tictactoe'
 require './lib/player'
 require './lib/board'
+
 describe TicTacToe do
   describe "#switch_players" do
     subject(:game_player) { described_class.new }
-    context "@current_player is @player_x" do
+
+    context "when @current_player is @player_x" do
       it "switches @current_player from @player_x to @player_o" do
         expect { game_player.switch_players }.to change { game_player.instance_variable_get(:@current_player) }
       end
     end
-    context "@current_player is @player_o" do
+
+    context "when @current_player is @player_o" do
       before do
         game_player.instance_variable_set(:@current_player, @player_o)
       end
